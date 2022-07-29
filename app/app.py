1ea9cd47bef77fe82b06ad92669b998994766d26
Bhagwat Sapkota <54609817+bhagwatsapkota@users.noreply.github.com>
2022-07-29 11:50:40 -0400

    Update app.py

--- app/app.py
+++ app/app.py
@@ -1,22 +1,13 @@
 from flask import Flask
 
-import index
-import intro
 import os
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return "<p>{}!</p>".format(index.get_hello())
+    return "Hello World!! This is Bhagwat's test!"
 
-
-@app.route("/adan")
-def adan_route():
-    return "<h1>{}<h1>".format(intro.get_message())
-
-def main() -> None:
-    print("{}".format(index.get_hello()))
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
